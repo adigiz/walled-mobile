974072b89bd67fc7ead68a02b7064281fcaffba2
Adityo <adigiz@yahoo.com>
2024-12-16 16:31:41 +0700

    feat: use async storage and navigate to login after setting

--- app/(home)/index.jsx
+++ app/(home)/index.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
 export default function Home() {
+  const [user, setUser] = useState({})
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const value = await AsyncStorage.getItem("token");
+        if (value !== null) {
+          const res = await axios.get(
+            "https://6776-182-3-53-7.ngrok-free.app/profile",
+            {
+              headers: {
+                Authorization: `Bearer ${value}`,
+              },
+            }
+          );
+          const user = res.data.data
+          setUser(user)
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getData();
+  }, []);
   return (
     <View style={styles.container}>
+      {user?.fullname && <Text>{user.fullname}</Text>}
       <Text>Home Screen</Text>
     </View>
   );
--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, TextInput, Image, Text } from "react-native";
 import Button from "../components/Button";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import { z } from "zod";
 import { useState } from "react";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const LoginSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
-  password: z.string().min(8, { message: "Must be 8 or more characters long" }),
+  password: z.string().min(4, { message: "Must be 4 or more characters long" }),
 });
 
 export default function App() {
   const [form, setForm] = useState({ email: "", password: "" });
   const [errorMsg, setErrors] = useState({});
+  const [serverError, setServerError] = useState("");
 
   const handleInputChange = (key, value) => {
     setForm({ ...form, [key]: value });
@@ -25,21 +28,44 @@ export default function App() {
     }
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     try {
-      LoginSchema.parse(form); 
+      LoginSchema.parse(form);
+
+      const res = await axios.post(
+        "https://6776-182-3-53-7.ngrok-free.app/auth/login",
+        form
+      );
+      await AsyncStorage.setItem("token", res.data.data.token);
+      router.replace("/(home)")
     } catch (err) {
-      const errors = {};
-      err.errors.forEach((item) => {
-        const key = item.path[0];
-        errors[key] = item.message;
-      });
-      setErrors(errors);
+      if (axios.isAxiosError(err)) {
+        if (err.response) {
+          setServerError(err.response.data.message || "An error occurred");
+        } else if (err.request) {
+          setServerError("Network error. Please try again later.");
+          console.error("Network Error:", err.request);
+        } else {
+          setServerError("An unexpected error occurred.");
+          console.error("Request Setup Error:", err.message);
+        }
+      } else if (err?.errors) {
+        const errors = {};
+        err.errors.forEach((item) => {
+          const key = item.path[0];
+          errors[key] = item.message;
+        });
+        setErrors(errors);
+      } else {
+        setServerError("An unknown error occurred.");
+        console.error("Unhandled Error:", err);
+      }
     }
   };
 
   return (
     <View style={styles.container}>
+      {serverError && <Text>{serverError}</Text>}
       <Image
         source={require("../assets/logo.png")}
         style={styles.logo}
@@ -47,30 +73,29 @@ export default function App() {
       />
 
       <TextInput
-        style={[
-          styles.input,
-          errorMsg.email && styles.inputError,
-        ]}
+        style={[styles.input, errorMsg.email && styles.inputError]}
         placeholder="Email"
         placeholderTextColor="#aaa"
+        autoCapitalize="none"
         keyboardType="email-address"
         onChangeText={(text) => handleInputChange("email", text)}
         value={form.email}
       />
-      {errorMsg.email ? <Text style={styles.errorMsg}>{errorMsg.email}</Text> : null}
+      {errorMsg.email ? (
+        <Text style={styles.errorMsg}>{errorMsg.email}</Text>
+      ) : null}
 
       <TextInput
-        style={[
-          styles.input,
-          errorMsg.password && styles.inputError,
-        ]}
+        style={[styles.input, errorMsg.password && styles.inputError]}
         placeholder="Password"
         placeholderTextColor="#aaa"
         secureTextEntry={true}
         onChangeText={(text) => handleInputChange("password", text)}
         value={form.password}
       />
-      {errorMsg.password ? <Text style={styles.errorMsg}>{errorMsg.password}</Text> : null}
+      {errorMsg.password ? (
+        <Text style={styles.errorMsg}>{errorMsg.password}</Text>
+      ) : null}
 
       <Link href="/(home)" style={styles.linkText}>
         Masuk
